89c452f4615cf56440f6f320d9715867b87fec85
manuijaev <kenyaniemmanuel44@gmail.com>
2024-09-20 23:36:31 +0300

    created a database,retrieved and filtered data

--- answers.sql
+++ answers.sql
@@ -1,2 +1,11 @@
 SELECT first_name,last_name,date_of_birth from patients;
+
 SELECT provider_id,first_name,provider_speciality from providers;
+
+SELECT *
+FROM hospital_db.patients
+WHERE first_name like 'Mw%';
+
+SELECT *
+FROM hospital_db.providers
+WHERE provider_speciality like '%y';
